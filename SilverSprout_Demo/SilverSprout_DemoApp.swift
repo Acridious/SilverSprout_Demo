36263c74939f6fb5f07a33cf0949bdaaa3841e02
Daniel Duong <hiroduongdaniel2@gmail.com>
2023-05-04 15:01:15 +0800

    Initial Commit

---
+++ SilverSprout_Demo/SilverSprout_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilverSprout_DemoApp.swift
+//  SilverSprout_Demo
+//
+//  Created by Daniel Duong on 4/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct SilverSprout_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
